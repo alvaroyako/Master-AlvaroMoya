c4e9b65cb68f24fe518d8478c5e5bee9c7557920
alvaroyako <alvaroyako2000@hotmail.com>
2021-10-18 14:16:46 +0200

    actualizacion

---
+++ REACT/segundoreact/src/components/SumarNumeros/SumarNumeros.js
@@ -0,0 +1,21 @@
+import duro from '/Programacion-AlvaroMoyaHerraiz/REACT/segundoreact/src/assets/images/duro.jpg';
+
+function SumarNumeros() {
+
+  const Sumar = (n1, n2) => {
+    var resultado = n1 + n2;
+    console.log(resultado);
+  };
+
+  return (
+    <div>
+      <h1 style={{ color: "red", backgroundColor: "green" }}>
+        Suma de dos numeros
+      </h1>
+      <button onClick={() => Sumar(7,3)}>Suma los numeros</button>
+      <img src={duro}></img>
+    </div>
+  );
+}
+
+export default SumarNumeros;
